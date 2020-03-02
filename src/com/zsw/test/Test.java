f706de69c5c80ea776ea86759641273da71ae3bb
ZHANGDOUBLEWEI <1390429173@qq.com>
2020-03-02 15:41:01 +0800

    2019.3.2我又测试了一下

--- src/com/zsw/test/Test.java
+++ src/com/zsw/test/Test.java
@@ -3,6 +3,6 @@ package com.zsw.test;
 public class Test {
     public static void main(String args[]){
         System.out.println("love tyn");
-        System.out.println("foreverforever");
+        System.out.println("foreverforeverforever");
     }
 }
